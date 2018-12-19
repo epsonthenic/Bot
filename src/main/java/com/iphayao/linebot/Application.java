7ee79de1d69d9ddf73c13d3a5529f710a72f3681
Yotsapohn <epson0882@gmail.com>
2018-12-19 23:16:12 +0700

    asd

--- src/main/java/com/iphayao/linebot/Application.java
+++ src/main/java/com/iphayao/linebot/Application.java
@@ -6,20 +6,26 @@ import com.linecorp.bot.model.message.Message;
 import com.linecorp.bot.model.message.TextMessage;
 import com.linecorp.bot.spring.boot.annotation.EventMapping;
 import com.linecorp.bot.spring.boot.annotation.LineMessageHandler;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 @LineMessageHandler
 public class Application {
+    private Logger LOGGER = LoggerFactory.getLogger(this.getClass());
+
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
 
     @EventMapping
     public Message handleTextMessage(MessageEvent<TextMessageContent> e) {
+        LOGGER.info("0000000000000000000 :");
         System.out.println("event: " + e);
         System.out.println("asdasdasdasd" + e);
+        LOGGER.info("firstName : {}",e);
         TextMessageContent message = e.getMessage();
         return new TextMessage(message.getText());
     }
